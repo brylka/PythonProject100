5462fc47f0a46d153b42fc4a5403cae03cb16083
Bartosz Bryniarski <brylka@jelenia.com>
2026-04-18 11:38:39 +0200

    Sobota

---
+++ a.py
@@ -0,0 +1,4 @@
+a = int(input("Liczba 1: "))
+b = int(input("Liczba 2: "))
+suma = a + b
+print(suma)
--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return "Cześć MERITO!!!!!!!!!!!!!!<br>18.04.2026"
+    a = b = suma = None
+    if request.method == 'POST':
+        a = request.form['a']
+        b = request.form['b']
+        suma = int(a) + int(b)
+
+    return render_template("index.html", a=a, b=b, s=suma)
 
 
 if __name__ == '__main__':
